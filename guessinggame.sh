602ca2d90da58d029e05b3bc56e7cf8d0946b783
tmkreddy <tmkrishnareddy@gmail.com>
2018-04-21 16:59:52 +0530

    made changes to bash file

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,17 +1,21 @@
+#!/usr/bin/env bash
+# File: guessinggame.sh
+
 function guessgame {
 tes=0
+tfiles=$(ls -l | grep "^-" | wc -l)
 while [[ tes -eq 0 ]]
 do 
   echo "how many files are there in current directory?"
   read nfiles
-   if [[ "$nfiles" -gt 3  ]]
+   if [[ $nfiles -gt $tfiles  ]]
    then
-    echo "too high"
-   elif [[ "$nfiles" -lt 3 ]]
+    echo "too high guess again"
+   elif [[ $nfiles -lt $tfiles ]]
    then
-    echo "too low"
+    echo "too low guess again"
    else
-    echo "congrats"
+    echo "congrats,you guessed correctly"
     tes=1
    fi
 done
